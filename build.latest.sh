b0c94c2056cab612c5bdce3a38c0d710f4cc136f
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-10-24 14:36:34 +0900

    update builds

--- build.latest.sh
+++ build.latest.sh
@@ -6,15 +6,17 @@
 set -x
 
 UBUNTU_VER=22.04
-ROS_DISTRO=one
+_ROS_DISTRO=one
 BASEIMAGE_TAG=12.1.0-cudnn8-devel-ubuntu${UBUNTU_VER}
 CUDA_IMAGE=nvidia/cuda:${BASEIMAGE_TAG}
 REPO=repo.irsl.eiiris.tut.ac.jp/
-IRSL_TAG=_latest
 # IRSL_TAG=''
-IRSL_BASE_IMAGE_NAME=${REPO}irsl_base:cuda_${BASEIMAGE_TAG}_${ROS_DISTRO}
-IRSL_SYSTEM_IMAGE_NAME=${REPO}irsl_system:cuda_${BASEIMAGE_TAG}_${ROS_DISTRO}
-OUTPUT_IMAGE_NAME=${REPO}genesis_with_irsl${IRSL_TAG}:cuda_${BASEIMAGE_TAG}_${ROS_DISTRO}
+# IRSL_TAG=_latest
+IRSL_TAG=_new_rsl_rl
+IRSL_BASE_IMAGE_NAME=${REPO}irsl_base:cuda_${BASEIMAGE_TAG}_${_ROS_DISTRO}
+IRSL_SYSTEM_IMAGE_NAME=${REPO}irsl_system:cuda_${BASEIMAGE_TAG}_${_ROS_DISTRO}
+IRSL_SYSTEM_IMAGE_NAME=${REPO}irsl_system:${_ROS_DISTRO}
+OUTPUT_IMAGE_NAME=${REPO}genesis_with_irsl${IRSL_TAG}:${_ROS_DISTRO}
 
 DOCKER_OPT='--progress plain'
 
@@ -27,7 +29,7 @@ else
 fi
 if [ -z "${FIND_IMAGE}" ]; then
     cd .irsl_docker_irsl_system/
-    INPUT_IMAGE=${IRSL_BASE_IMAGE_NAME} BUILD_ROS=${ROS_DISTRO} BUILD_UBUNTU=${UBUNTU_VER} ./build.sh ${IRSL_SYSTEM_IMAGE_NAME}
+    INPUT_IMAGE=${IRSL_BASE_IMAGE_NAME} BUILD_ROS=${_ROS_DISTRO} BUILD_UBUNTU=${UBUNTU_VER} ./build.sh ${IRSL_SYSTEM_IMAGE_NAME}
     cd ..
 fi
 
--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+IRSL_TAG=_new_rsl_rl
+# IRSL_TAG=_latest
+# IRSL_TAG=''
+
+_UBUNTU_VER=22.04
+_ROS_DISTRO=one
 REPO=repo.irsl.eiiris.tut.ac.jp/
-UBUNTU_VER=22.04
-ROS_DISTRO=one
-BASEIMAGE_TAG=12.1.0-cudnn8-devel-ubuntu${UBUNTU_VER}
-CUDA_IMAGE=nvidia/cuda:${BASEIMAGE_TAG}
-REPO=repo.irsl.eiiris.tut.ac.jp/
-IRSL_TAG=_latest
-#IRSL_TAG=''
-IRSL_BASE_IMAGE_NAME=${REPO}irsl_base:cuda_${BASEIMAGE_TAG}_${ROS_DISTRO}
-IRSL_SYSTEM_IMAGE_NAME=${REPO}irsl_system:cuda_${BASEIMAGE_TAG}_${ROS_DISTRO}
-OUTPUT_IMAGE_NAME=${REPO}genesis_with_irsl${IRSL_TAG}:cuda_${BASEIMAGE_TAG}_${ROS_DISTRO}
+IRSL_SYSTEM_IMAGE_NAME=${REPO}irsl_system:22.04_one
+OUTPUT_IMAGE_NAME=${REPO}genesis_with_irsl${IRSL_TAG}:22.04_one
 
 # .irsl_docker_irsl_system/run.sh --image ${OUTPUT_IMAGE_NAME} -w ${PWD}/userdir${IRSL_TAG} "$@"
 .irsl_docker_irsl_system/run.sh --image ${OUTPUT_IMAGE_NAME} -w ${PWD} "$@"
